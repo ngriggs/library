4d016bbb9d8a696df2aeddf7e6ebfaff19ed65aa
ngriggs <noahg@vt.edu>
2021-05-31 20:44:24 -0500

    still need to add the ability to remove books, need to also fix the issue where opening the submit form multiple times will also submit the data multiple times.

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const gridContainer = document.querySelector('.gridContainer');
 
 const newBookButton = document.querySelector('.newBook');
 
-newBookButton.addEventListener("click", newBook);
+const submitButton = document.querySelector('.btn');
+
+
+
+newBookButton.addEventListener("click", openForm);
+
+
 
 function newBook() {
-    openForm()
-    
-    // let title = prompt("Please enter the title of your book");
-    // let author = prompt("Please enter the author of your book");
-    // let text = new book(title, author)
-    // addBookToLibrary(text, myLibrary)
-    displayBooks(myLibrary)
+    const form = document.forms[0];
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const { title, author, pages, read } = this.elements;
+        let text = new book(title.value, author.value, pages.value, read.value);
+        addBookToLibrary(text, myLibrary)
+        displayBooks(myLibrary)
+      });
+
 }
 
 function book(title, author, ...args) {
@@ -31,22 +39,38 @@ function addBookToLibrary(book, myLibrary) {
     myLibrary.push(book);
 };
 
-
+function emptyDisplay() {
+    var node = document.querySelector('.gridContainer');
+    node.querySelectorAll('*').forEach(n => n.remove());
+}
 
 function displayBooks(myLibrary) {
-
+    emptyDisplay()
     for (i=0; i < myLibrary.length; i++) {
         const bookDiv = document.createElement('div');
+        const removeButton = document.createElement('button');
+        removeButton.setAttribute('class', 'removeButton')
+        removeButton.setAttribute('id', i);
+        removeButton.innerHTML = 'Remove'
         bookDiv.classList.add('bookCard');
         bookDiv.innerHTML += '<h3>'+ myLibrary[i].title+'</h3>';
         bookDiv.innerHTML += '<p>'+ myLibrary[i].author+'</p>';
+        bookDiv.appendChild(removeButton)
+        bookDiv.setAttribute('id', i)
         gridContainer.appendChild(bookDiv);
     };
+
 };
 
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1)
+    console.log(myLibrary)
+    displayBooks(myLibrary)
+}
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    newBook()
 }
 
 function closeForm() {
@@ -54,13 +78,6 @@ function closeForm() {
 } 
 
 
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien')
+const removeBtn = document.getElementsByClassName('removeButton')
+removeBtn.addEventListener('click', removeBookFromLibrary(0))
 
-for (i = 0; i < 10; i++) {
-    let text = new book(' '+i, ' '+i)
-    addBookToLibrary(text, myLibrary)
-}
-
-addBookToLibrary(theHobbit, myLibrary)
-displayBooks(myLibrary)
-console.log(myLibrary[0].title)
